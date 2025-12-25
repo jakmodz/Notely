5774759ad3ec94a9ab37ee6493821a80ef947a12
Kuba <144469844+jakmodz@users.noreply.github.com>
2025-12-25 20:42:57 +0100

    feat: add handler for MethodArgumentNotValidException to return validation error responses

--- backend/src/main/java/io/github/jakmodz/backend/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/io/github/jakmodz/backend/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package io.github.jakmodz.backend.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -17,10 +18,11 @@ public class GlobalExceptionHandler {
                 HttpStatus.CONFLICT.value(),
                 HttpStatus.CONFLICT.toString(),
                 e.getMessage(),
-                request.getDescription(false).replace("uri=","")
+                request.getDescription(false).replace("uri=", "")
         );
         return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
     }
+
     @ExceptionHandler(ExpiredRefreshToken.class)
     public ResponseEntity<ErrorResponse> handleExpiredRefreshToken(ExpiredRefreshToken e, WebRequest request) {
         ErrorResponse error = new ErrorResponse(
@@ -28,8 +30,21 @@ public class GlobalExceptionHandler {
                 HttpStatus.UNAUTHORIZED.value(),
                 HttpStatus.UNAUTHORIZED.toString(),
                 e.getMessage(),
-                request.getDescription(false).replace("uri=","")
+                request.getDescription(false).replace("uri=", "")
         );
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException e, WebRequest request) {
+        String message = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.toString(),
+                message,
+                request.getDescription(false).replace("uri=", "")
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }

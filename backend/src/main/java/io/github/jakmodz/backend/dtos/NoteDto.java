8d9852a06d954482a68094b009efdf0788d4b827
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-01-06 21:49:38 +0100

    fix(NoteDto): change of created instead of update date

--- backend/src/main/java/io/github/jakmodz/backend/dtos/NoteDto.java
+++ backend/src/main/java/io/github/jakmodz/backend/dtos/NoteDto.java
@@ -1,16 +1,20 @@
 package io.github.jakmodz.backend.dtos;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class NoteDto {
     private UUID uuid;
+    @NotBlank
     private String title;
     private String content;
-    private LocalDateTime updated;
+    private LocalDateTime created;
 }

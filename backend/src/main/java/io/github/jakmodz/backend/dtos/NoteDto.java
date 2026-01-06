d29d5e070c3dd89a0e3f07d78657a2ff3857e11d
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-01-06 12:56:02 +0100

    feat(note): add UUID and updated timestamp fields to NoteDto

--- backend/src/main/java/io/github/jakmodz/backend/dtos/NoteDto.java
+++ backend/src/main/java/io/github/jakmodz/backend/dtos/NoteDto.java
@@ -3,9 +3,14 @@ package io.github.jakmodz.backend.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 public class NoteDto {
+    private UUID uuid;
     private String title;
     private String content;
+    private LocalDateTime updated;
 }

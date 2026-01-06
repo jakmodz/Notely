5e2026132cdaf2ca2f07925d613aa09abf76065e
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-01-06 21:48:09 +0100

    fix: update User entity to user Lazy fetch

--- backend/src/main/java/io/github/jakmodz/backend/models/User.java
+++ backend/src/main/java/io/github/jakmodz/backend/models/User.java
@@ -25,8 +25,9 @@ public class User {
     String username;
     String password;
 
-    @OneToMany
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     Set<Note> notes;
+
 
     @CreationTimestamp
     LocalDateTime created;

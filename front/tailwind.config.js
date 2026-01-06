be0841fad7bed5620c9b1c0e63efbbdea86b36ff
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-01-06 12:57:10 +0100

    feat(dashboard): add typography plugin and custom styles for improved markdown rendering

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -23,8 +23,100 @@ module.exports = {
           dark: '#3b82f6',
         },
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.red'),
+            a: {
+              color: theme('colors.emerald.600'),
+              '&:hover': {
+                color: theme('colors.emerald.700'),
+              },
+            },
+            strong: {
+              color: theme('colors.slate.900'),
+            },
+            code: {
+              color: theme('colors.emerald.600'),
+            },
+            h1: {
+              color: theme('colors.slate.900'),
+            },
+            h2: {
+              color: theme('colors.slate.900'),
+            },
+            h3: {
+              color: theme('colors.slate.900'),
+            },
+            h4: {
+              color: theme('colors.slate.900'),
+            },
+            ul: {
+              listStyleType: 'disc',
+              paddingLeft: '1.5em',
+            },
+            ol: {
+              listStyleType: 'decimal',
+              paddingLeft: '1.5em',
+            },
+            'ul > li': {
+              paddingLeft: '0.375em',
+            },
+            'ol > li': {
+              paddingLeft: '0.375em',
+            },
+            'ul > li::marker': {
+              color: theme('colors.emerald.600'),
+            },
+            'ol > li::marker': {
+              color: theme('colors.emerald.600'),
+            },
+          },
+        },
+        invert: {
+          css: {
+            color: theme('colors.slate.50'),
+            a: {
+              color: theme('colors.emerald.400'),
+              '&:hover': {
+                color: theme('colors.emerald.300'),
+              },
+            },
+            strong: {
+              color: theme('colors.white'),
+            },
+            code: {
+              color: theme('colors.emerald.400'),
+            },
+            h1: {
+              color: theme('colors.white'),
+            },
+            h2: {
+              color: theme('colors.white'),
+            },
+            h3: {
+              color: theme('colors.white'),
+            },
+            h4: {
+              color: theme('colors.white'),
+            },
+            blockquote: {
+              color: theme('colors.slate.400'),
+              borderLeftColor: theme('colors.emerald.500'),
+            },
+            'ul > li::marker': {
+              color: theme('colors.emerald.400'),
+            },
+            'ol > li::marker': {
+              color: theme('colors.emerald.400'),
+            },
+          },
+        },
+      }),
     },
   },
 
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }

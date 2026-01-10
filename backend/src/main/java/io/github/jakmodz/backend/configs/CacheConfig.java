92f926e914c5c84311b321146b3868172642063e
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-01-10 18:28:50 +0100

    refactor: remove unused imports and clean up code in backend classes

--- backend/src/main/java/io/github/jakmodz/backend/configs/CacheConfig.java
+++ backend/src/main/java/io/github/jakmodz/backend/configs/CacheConfig.java
@@ -1,8 +1,5 @@
 package io.github.jakmodz.backend.configs;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
--- backend/src/main/java/io/github/jakmodz/backend/models/User.java
+++ backend/src/main/java/io/github/jakmodz/backend/models/User.java
@@ -1,6 +1,5 @@
 package io.github.jakmodz.backend.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;

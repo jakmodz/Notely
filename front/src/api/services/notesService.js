d2819c9056e2f2087b41a7a48d0d6653dd2a0bea
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-01-12 22:54:10 +0100

    feat: update getAllNotes to include search parameter for improved note retrieval

--- front/src/api/services/notesService.js
+++ front/src/api/services/notesService.js
@@ -4,11 +4,12 @@ export default{
   getNote(uuid){
     return apiClient.get(`/notes/${uuid}`);
   },
-  getAllNotes(page = 0, size = 12, sortBy = 'created', sortDirection = 'DESC',searchValue=''){
+  getAllNotes(page = 0, size = 12, sortBy = 'created',  sortDirection = 'DESC',search = ''){
     return apiClient.get('/notes', {
-      params: { page, size, sortBy, sortDirection }
+      params: { page, size, sortBy, sortDirection, searchValue: search }
     });
   },
+
   deleteNote(uuid){
     return apiClient.delete(`/notes/${uuid}`);
   },

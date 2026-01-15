894490fa801c585da8a71fd8cad10690aba5658d
Kuba <144469844+jakmodz@users.noreply.github.com>
2026-01-15 17:16:28 +0100

    fix: remove unnecessary blank line in AuthEntryPointJwt.java

--- backend/src/main/java/io/github/jakmodz/backend/jwt/AuthEntryPointJwt.java
+++ backend/src/main/java/io/github/jakmodz/backend/jwt/AuthEntryPointJwt.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
-
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
             throws IOException, ServletException {
